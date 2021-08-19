6979df8406a9241d77ddbbff4c6376e0d446fd39
StevenM42 <EveningDragon-64>
2021-08-19 18:24:29 +1000

    Refactor score status program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -4,12 +4,20 @@ Fixed program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-if 100 < score > 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(rate_score(score))
+
+
+def rate_score(score):
+    if 100 < score > 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
