d5047845c164abc9ca13e98944edbb4fd8923051
StevenM42 <EveningDragon-64>
2021-08-19 16:27:49 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,10 +1,19 @@
 """Password check program that returns asterisks of password length"""
 
-Minimum_character_limit = 6
 
-password = input("Please enter password at least {} characters long: ".format(Minimum_character_limit))
+def main():
+    minimum_character_limit = 6
 
-while len(password) < Minimum_character_limit:
-    password = input("Please enter password at least {} characters long: ".format(Minimum_character_limit))
+    password = get_password(minimum_character_limit)
 
-print('*' * len(password))
+    print('*' * len(password))
+
+
+def get_password(minimum_character_limit):
+    password = input("Please enter password at least {} characters long: ".format(minimum_character_limit))
+    while len(password) < minimum_character_limit:
+        password = input("Please enter password at least {} characters long: ".format(minimum_character_limit))
+    return password
+
+
+main()
