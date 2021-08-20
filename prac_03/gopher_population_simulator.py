6482625f20614f0e2bcd00fcd369078ba74e6d6c
StevenM42 <EveningDragon-64>
2021-08-20 14:36:43 +1000

    Change random number generation method to integers

--- prac_03/gopher_population_simulator.py
+++ prac_03/gopher_population_simulator.py
@@ -7,17 +7,17 @@ import random
 population = 1000
 total_years = 10
 current_year = 0
-min_birth_rate = 0.1
-max_birth_rate = 0.2
-min_death_rate = 0.05
-max_death_rate = 0.25
+min_birth_rate = 10
+max_birth_rate = 20
+min_death_rate = 5
+max_death_rate = 25
 
 print('Welcome to the Gopher Population Simulator!')
 
 while total_years > current_year:
     current_year += 1
-    birth_rate = population * random.uniform(min_birth_rate, max_birth_rate)
-    death_rate = population * random.uniform(min_death_rate, min_death_rate)
+    birth_rate = population * (random.randint(min_birth_rate, max_birth_rate) / 100)
+    death_rate = population * (random.randint(min_death_rate, max_death_rate) / 100)
     population += birth_rate - death_rate
     print(f'{birth_rate:.0f} gophers were born. {death_rate:.0f} died.')
     print(f'Population: {population:.0f}')
