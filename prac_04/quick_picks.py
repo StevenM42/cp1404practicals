fb27c9228c7fc240fc265eefbc7c1af696045a7b
StevenM42 <EveningDragon-64>
2021-08-26 21:13:23 +1000

    Add quick pick program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical
+quick picks program
+"""
+
+import random
+
+NUMBER_PER_LINE = 6
+MIN = 1
+MAX = 45
+
+number_of_picks = int(input("How many quick picks? "))
+
+for i in range(number_of_picks):
+    picks = []
+    for j in range(NUMBER_PER_LINE):
+        number = random.randint(MIN, MAX)
+        while number in picks:
+            number = random.randint(MIN, MAX)
+        picks.append(number)
+    picks.sort()
+    print(picks)
