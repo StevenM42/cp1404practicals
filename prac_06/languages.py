9db13d30078f93a15f39279c7411ebfdcaa3bf42
StevenM42 <EveningDragon-64>
2021-09-10 14:12:01 +1000

    Add class and client code

---
+++ prac_06/languages.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+Programming Language client side code.
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+
+
+main()
