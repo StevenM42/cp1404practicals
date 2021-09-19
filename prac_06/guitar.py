dc2eec7df71f81633c28f37a5923a0b0d85d4904
StevenM42 <EveningDragon-64>
2021-09-19 12:02:09 +1000

    Initial program before testing

---
+++ prac_06/guitar.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Guitar class
+"""
+
+CURRENT_YEAR = 2021
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Class for storing data and checking vintage of guitars"""
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() > VINTAGE_AGE
+
