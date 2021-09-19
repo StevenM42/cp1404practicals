924f6aeefe1cb93c6bcbf68a2ee66bad86aa1622
StevenM42 <EveningDragon-64>
2021-09-19 12:38:46 +1000

    Add display list when program is done

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,11 +14,21 @@ def main():
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
-        cost = int(input("Cost: $"))
+        cost = float(input("Cost: $"))
         guitar_to_add = Guitar(name, year, cost)
         guitars.append(guitar_to_add)
         print(guitar_to_add, "added")
         name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        vintage_status = ""
+        if guitar.is_vintage():
+            vintage_status = "vintage"
+        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_status}")
 
 
 main()
