8d2bd05cb20348dd828df8aa206832a3768c5bc8
StevenM42 <EveningDragon-64>
2021-09-25 19:17:51 +1000

    Add reset capability to clear button

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -9,7 +9,12 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print("greet")
+        print("test")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def reset(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
