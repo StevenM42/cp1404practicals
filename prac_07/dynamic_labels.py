f4468c813c149c95a5b6d88b5b03c7781c6c9aac
StevenM42 <EveningDragon-64>
2021-09-26 12:56:50 +1000

    Create simple label display GUI

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practical 7
+
+"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name_to_phone = {"Bob Brown", "Cat Cyan", "Oren Ochre"}
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.name_to_phone:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
