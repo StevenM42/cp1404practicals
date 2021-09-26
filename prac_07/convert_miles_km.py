e051c749df5635f4ad8c03095d3714559ed27e38
StevenM42 <EveningDragon-64>
2021-09-26 12:29:42 +1000

    Create initial gui

---
+++ prac_07/convert_miles_km.py
@@ -0,0 +1,15 @@
+
+from kivy.app import App
+from kivy.lang import Builder
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverter(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+MilesConverter().run()
