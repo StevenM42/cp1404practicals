4f786bc2e58f25031b19f8c078117414558f9e47
StevenM42 <steven.moore@my.jcu.edu.au>
2021-10-08 13:21:23 +1000

    Move price per kilometer variable into taxi class

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -8,7 +8,7 @@ from prac_08.taxi import Taxi
 
 def main():
     """Taxi class test"""
-    taxi_1 = Taxi("Prius 1", 100, 1.23)
+    taxi_1 = Taxi("Prius 1", 100)
     taxi_1.drive(40)
     print(taxi_1)
     taxi_1.start_fare()
